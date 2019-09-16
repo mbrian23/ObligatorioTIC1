a2be6ad4aa62a4c4e739802dc84dd42a24190187
diegogibert <diegoogibert@gmail.com>
2019-09-16 17:28:51 -0300

    Base con metodos basicos en pelicula

--- entidadesTic/src/main/java/org/hibernate/jpa/entidadesTic/Entity/Pelicula/Pelicula.java
+++ entidadesTic/src/main/java/org/hibernate/jpa/entidadesTic/Entity/Pelicula/Pelicula.java
@@ -29,5 +29,57 @@ public class Pelicula {
 
     @NotBlank
     @Column(nullable = false, length = 45)
-    private String Descripcion;
+    private String descripcion;
+
+
+    public Pelicula() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- entidadesTic/src/main/java/org/hibernate/jpa/entidadesTic/Entity/Pelicula/PeliculaRepository.java
+++ entidadesTic/src/main/java/org/hibernate/jpa/entidadesTic/Entity/Pelicula/PeliculaRepository.java
@@ -1,7 +1,9 @@
 package org.hibernate.jpa.entidadesTic.Entity.Pelicula;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeliculaRepository extends CrudRepository<Pelicula, Long> {
 
 }
