76276938307a6e132b3982a66efe2dcf2d94131a
jstaricco10 <jstaricco@correo.um.edu.uy>
2019-09-16 19:18:38 -0300

    actualizaciontodo

--- javafx/src/main/java/com/example/moviecrud/MovieCrudApplication.java
+++ javafx/src/main/java/com/example/moviecrud/MovieCrudApplication.java
@@ -4,12 +4,17 @@ package com.example.moviecrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MovieCrudApplication {
+    private static ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(MovieCrudApplication.class, args);
     }
 
+    public static ConfigurableApplicationContext getContext() {
+        return context;
+    }
 }
--- javafx/src/main/java/com/example/moviecrud/ui/Principal.java
+++ javafx/src/main/java/com/example/moviecrud/ui/Principal.java
@@ -1,4 +1,30 @@
 package com.example.moviecrud.ui;
 
+import com.example.moviecrud.MovieCrudApplication;
+import com.example.moviecrud.business.entities.Pelicula;
+import com.example.moviecrud.ui.movie.MovieController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Principal {
+    @FXML
+    private Pelicula ItemAgregarPelicula;
+
+    @FXML
+    void agregarClientAction(ActionEvent event) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(MovieCrudApplication.getContext()::getBean);
+
+        Parent root = fxmlLoader.load(MovieController.class.getResourceAsStream("Principal.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
