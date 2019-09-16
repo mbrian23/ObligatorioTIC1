ced330a6f13eb1b26db2ff18dd94fbfb4b72ce7a
diegogibert <diegoogibert@gmail.com>
2019-09-16 20:11:15 -0300

    Tira la pantalla de agregar pelicula, todavia no las sube a la base

--- javafx/src/main/java/com/example/moviecrud/MovieCrudApplication.java
+++ javafx/src/main/java/com/example/moviecrud/MovieCrudApplication.java
@@ -2,19 +2,51 @@ package com.example.moviecrud;
 
 
 
+import com.example.moviecrud.ui.Principal;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class MovieCrudApplication {
+public class MovieCrudApplication extends Application {
+
     private static ConfigurableApplicationContext context;
 
+    private Parent root;
+
+    @Override
+    public void init() throws Exception {
+        MovieCrudApplication.context = SpringApplication.run(MovieCrudApplication.class);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(MovieCrudApplication.getContext()::getBean);
+
+
+        root = fxmlLoader.load(Principal.class.getResourceAsStream("Principal.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    @Override
+    public void stop() {
+        MovieCrudApplication.getContext().close();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(MovieCrudApplication.class, args);
+        launch(args);
     }
 
     public static ConfigurableApplicationContext getContext() {
         return context;
     }
 }
+
--- javafx/src/main/java/com/example/moviecrud/ui/movie/MovieController.java
+++ javafx/src/main/java/com/example/moviecrud/ui/movie/MovieController.java
@@ -1,9 +1,14 @@
 package com.example.moviecrud.ui.movie;
 
 import com.example.moviecrud.business.PeliculaMgr;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,5 +28,28 @@ public class MovieController {
     private TextField titulo;
 
     @FXML
-    private Tex
+    private TextField actores;
+
+    @FXML
+    private ComboBox<String> genero;
+
+    @FXML
+    private TextField duracion;
+
+    @FXML
+    private TextArea descripcion;
+
+    @FXML
+    void close(ActionEvent actionEvent) {
+        Node source = (Node)  actionEvent.getSource();
+        Stage stage  = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void addClient(ActionEvent event) {
+
+    }
+
+
 }
